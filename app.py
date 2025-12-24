51dc36213acd1101fc06786f2a11faec0cde5ca5
akashpalaniradj <akashpalaniradj@gmail.com>
2025-12-24 12:11:14 +0530

    Add prometheus

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
-from flask import Flask
+from flask import Flask, Response
+from prometheus_client import Counter, Histogram, generate_latest, CONTENT_TYPE_LATEST
+import time
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello from Jenkins CI/CD!"
+# Metrics
+REQUEST_COUNT = Counter(
+    'flask_http_requests_total',
+    'Total HTTP requests',
+    ['method', 'endpoint']
+)
 
-app.run(host="0.0.0.0", port=5000)
+REQUEST_LATENCY = Histogram(
+    'flask_http_request_duration_seconds',
+    'HTTP request latency',
+    ['endpoint']
+)
+
+@app.route("/")
+def home():
+    REQUEST_COUNT.labels(method='GET', endpoint='/').inc()
+    with REQUEST_LATENCY.labels(endpoint='/').time():
+        return "Hello from Jenkins with Prometheus Metrics 🚀"
+
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
